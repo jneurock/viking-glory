11c9f4b7570bc71e2762d41014d08423930e89e5
Rocky Neurock <James.Neurock@cambiahealth.com>
2014-08-27 14:05:35 -0700

    Updated Viking Base dependency to version 4. Still need to test fully. Output seems ok.

--- gulpfile.js
+++ gulpfile.js
@@ -5,82 +5,41 @@
  */
 
 var gulp = require('gulp'),
+    local = null,
     plugins = require('gulp-load-plugins')(),
     vb = require('./bower_components/viking-base/node_modules/viking-base');
 
-// Set module refeneces on vb object
-vb.gulp = gulp;
-vb.plugins = plugins;
+function build() {
 
-// Output updates
-vb.output.fonts = 'fonts/';
-vb.output.img = 'img/';
-vb.output.post = 'posts/';
-vb.output.posts = 'posts.json';
+  // Set module refeneces on vb object
+  vb.gulp = gulp;
+  vb.plugins = plugins;
 
-// Source updates
-vb.sources.css = 'bower_components/viking-base/css/**/*.css';
-vb.sources.fonts = 'fonts/**/*';
-vb.sources.img = 'img/**/*';
-vb.sources.posts = 'posts/**/*.md';
+  // Modify tasks as you need before calling vb.registerGulpTasks
+  vb.registerGulpTasks();
+}
 
-// Task updates
-vb.tasks.build.depends = [
-  'fonts',
-  'handlebars',
-  'img',
-  'js-doc',
-  'posts',
-  'root'
-];
+function doBuild() {
 
-vb.tasks.css = {
-  cb: function() {
+  try {
 
-    return gulp.src( this.sources.css )
-      .pipe( gulp.dest( this.output.publish + this.output.css ) );
+    local = require('./viking-base');
+
+    if ( typeof local !== 'function' ) {
+
+      throw 'Local Viking Base file must export a function';
+    }
+
+  } catch( ex ) {
+
+    // If no local Viking Base file, mimic the expected module output
+    local = function( gulp, plugins, vb, cb ) {
+
+      cb();
+    }
   }
-};
 
-vb.tasks.fonts = {
-  cb: function() {
+  local( gulp, plugins, vb, build );
+}
 
-    return gulp.src( this.sources.fonts )
-      .pipe( gulp.dest( this.output.publish + this.output.fonts ) );
-  }
-};
-
-vb.tasks.handlebars.depends = ['img'];
-
-vb.tasks.img = {
-  cb: function() {
-
-    return gulp.src( this.sources.img )
-      .pipe( gulp.dest( this.output.publish + this.output.img ) );
-  }
-};
-
-vb.tasks.posts = {
-  cb: function() {
-
-    var concatOpts = {
-          newLine: ','
-        },
-        vbOpts = {
-          highlightSyntax: true
-        },
-        vbConcatOpts = {
-          concat: true
-        };
-
-    return gulp.src( this.sources.posts )
-      .pipe( plugins.vikingPosts( vbOpts ) )
-      .pipe( gulp.dest( this.output.publish + this.output.post ) )
-      .pipe( plugins.concat( this.output.posts, concatOpts ) )
-      .pipe( plugins.vikingPosts( vbConcatOpts ) )
-      .pipe( gulp.dest( this.output.publish + this.output.post ) );
-  }
-};
-
-// Modify tasks as you need before calling vb.registerGulpTasks
-vb.registerGulpTasks();
+doBuild();
